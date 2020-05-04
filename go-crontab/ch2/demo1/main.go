46477c8b9533abff9bc53b864bcedd677768478f
ftconan <1508907793@qq.com>
2020-05-04 21:59:35 +0800

    add go-crontab

---
+++ go-crontab/ch2/demo1/main.go
@@ -0,0 +1,20 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/5/4
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	var (
+		cmd *exec.Cmd
+		err error
+	)
+
+	cmd = exec.Command("/bin/bash", "-c", "echo 1;echo 2;")
+	err = cmd.Run()
+	fmt.Println(err)
+}

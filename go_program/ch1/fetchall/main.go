9be12fe7b8cfc9b26cd7eccd9aa0817d06ac4666
ftconan <1508907793@qq.com>
2020-05-05 16:47:45 +0800

    update fetchall

--- go_program/ch1/fetchall/main.go
+++ go_program/ch1/fetchall/main.go
@@ -21,7 +21,7 @@ func main() {
 	for range os.Args[1:] {
 		fmt.Println(<-ch) // receive form channel
 	}
-	fmt.Println("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
 func fetch(url string, ch chan<- string) {

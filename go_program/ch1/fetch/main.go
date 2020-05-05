7f47ff264f8f5988a31631f55b6e514df2ac8532
ftconan <1508907793@qq.com>
2020-05-05 15:59:08 +0800

    add fetch

---
+++ go_program/ch1/fetch/main.go
@@ -0,0 +1,28 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/5/5
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "fectch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		_ = resp.Body.Close()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "fecth: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Println("%s", string(b))
+	}
+}

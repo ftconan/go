914b832c6ffaa5d888653aab44bdb65bbf2d7dd9
ftconan <1508907793@qq.com>
2020-05-07 15:38:29 +0800

    add popcount

---
+++ go_program/ch2/cf/main.go
@@ -0,0 +1,25 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/5/7
+package main
+
+import (
+	"fmt"
+	"golang/go_program/ch2/tempconv"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
---
+++ go_program/ch2/tempconv/tempconv.go
@@ -0,0 +1,23 @@
+// Author: magician
+// File:   tempconv.go
+// Date:   2020/5/7
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F", f)
+}

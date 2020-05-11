799b7ac0550931bfd2a05c51d9b17f5e85ec6c47
ftconan <1508907793@qq.com>
2020-05-11 11:19:58 +0800

    add basename and printints

---
+++ go_program/ch3/comma/main.go
@@ -0,0 +1,24 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/5/11
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf(" %s\n", comma(os.Args[i]))
+	}
+}
+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
---
+++ go_program/ch3/printints/main.go
@@ -0,0 +1,27 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/5/11
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// intsToString is like fmt.Sprint(values) but adds commas.
+func intsToString(values []int) string {
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		_, _ = fmt.Fprintf(&buf, "%d", v)
+	}
+	buf.WriteByte(']')
+	return buf.String()
+}
+
+func main() {
+	fmt.Println(intsToString([]int{1, 2, 3})) // "[1, 2, 3]"
+}

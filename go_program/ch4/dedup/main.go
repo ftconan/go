2db2ded6c1f22e7e9de6df9175002c79e3d389d8
ftconan <1508907793@qq.com>
2020-05-13 09:25:03 +0800

    add charcount

---
+++ go_program/ch4/dedup/main.go
@@ -0,0 +1,27 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/5/12
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main()  {
+	seen := make(map[string]bool) // a set of strings
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		line := input.Text()
+		if !seen[line] {
+			seen[line] = true
+			fmt.Println(line)
+		}
+	}
+
+	if err := input.Err(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
+		os.Exit(1)
+	}
+}

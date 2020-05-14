dfb73fc73adaa665aba9c1afdf454ab288f7d07b
ftconan <1508907793@qq.com>
2020-05-14 10:45:45 +0800

    add github issues

---
+++ go_program/ch4/issue/main.go
@@ -0,0 +1,23 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/5/14
+package main
+
+import (
+"fmt"
+"golang/go_program/ch4/github"
+"log"
+"os"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n",
+			item.Number, item.User.Login, item.Title)
+	}
+}

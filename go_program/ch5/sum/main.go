3024de0eef2172ba29ecd5547d959c2b91805263
ftconan <1508907793@qq.com>
2020-06-17 10:46:50 +0800

    add sum

---
+++ go_program/ch5/sum/main.go
@@ -0,0 +1,24 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/6/17
+package main
+
+import "fmt"
+
+func sum(vals ...int) int {
+	total := 0
+	for _, val := range vals {
+		total += val
+	}
+	return total
+}
+
+func main() {
+	fmt.Println(sum())
+	fmt.Println(sum(3))
+	fmt.Println(sum(1, 2, 3, 4))
+
+	// slice
+	values := []int{1, 2, 3, 4}
+	fmt.Println(sum(values...))
+}

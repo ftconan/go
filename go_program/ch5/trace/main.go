a31ef30d1ad8c347ebd6659b8f46991492d6564a
ftconan <1508907793@qq.com>
2020-06-17 11:34:11 +0800

    add trace

---
+++ go_program/ch5/trace/main.go
@@ -0,0 +1,27 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/6/17
+package main
+
+import (
+"log"
+"time"
+)
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")()
+	// ...lots of work...
+	time.Sleep(10 * time.Second)
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit %s (%s)", msg, time.Since(start))
+	}
+}
+
+func main() {
+	bigSlowOperation()
+}

6522c5c4bef2cb2d703c31f04ecd9f28cd44fe45
ftconan <1508907793@qq.com>
2020-06-19 16:02:01 +0800

    add bytecounter

---
+++ go_program/ch7/bytecounter/main.go
@@ -0,0 +1,24 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/6/19
+package main
+
+import "fmt"
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	_, _ = c.Write([]byte("hello"))
+	fmt.Println(c)
+
+	c = 0
+	var name = "Dolly"
+	_, _ = fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+}

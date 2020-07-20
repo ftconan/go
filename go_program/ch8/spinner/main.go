cd39f65aa6219e9bd376fc27e200fc60ee578e32
ftconan <1508907793@qq.com>
2020-07-20 17:34:21 +0800

    add spinner

---
+++ go_program/ch8/spinner/main.go
@@ -0,0 +1,33 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/7/20
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	go spinner(100 * time.Microsecond)
+	const n = 45
+	fibN := fib(n) // slow
+	fmt.Printf("\nFibnacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int  {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
+

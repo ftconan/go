7570b88117022a8f011d2549c76a8d0b3670ca85
ftconan <1508907793@qq.com>
2020-07-21 11:44:13 +0800

    add netcat and reverb

---
+++ go_program/ch8/netcat1/netcat.go
@@ -0,0 +1,26 @@
+// Author: magician
+// File:   netcat.go
+// Date:   2020/7/21
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main()  {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go_program/ch8/reverb2/reverb.go
@@ -0,0 +1,45 @@
+// Author: magician
+// File:   reverb.go
+// Date:   2020/7/21
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	_, _ = fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	_, _ = fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	_, _ = fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}

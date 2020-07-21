5e6941911dbe345ef43245def608c435e5ece818
ftconan <1508907793@qq.com>
2020-07-21 14:45:31 +0800

    add pipeline and cake

---
+++ go_program/ch8/pipeline3/main.go
@@ -0,0 +1,35 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/7/21
+package main
+
+import "fmt"
+
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func square(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go counter(naturals)
+	go square(squares, naturals)
+	printer(squares)
+}

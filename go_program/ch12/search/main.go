47ad1f0a40c01661f625cc8b33e3fe6bc3407f59
ftconan <1508907793@qq.com>
2020-08-01 11:55:05 +0800

    add params

---
+++ go_program/ch12/search/main.go
@@ -0,0 +1,32 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/8/1
+package search
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// search implements the /search URL endpoint.
+func search(resp http.ResponseWriter, req *http.Request) {
+	var data struct {
+		Labels     []string `http:"l"`
+		MaxResults int      `http:"max"`
+		Exact      bool     `http:"x"`
+	}
+	data.MaxResults = 10 // set default
+	if err := params.Unpack(req, &data); err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest) // 400
+		return
+	}
+
+	// ...rest of handler...
+	_, _ = fmt.Fprintf(resp, "Search: %+v\n", data)
+}
+
+func main() {
+	http.HandleFunc("/search", search)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}

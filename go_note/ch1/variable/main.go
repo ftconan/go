712a69d77f5deb1f185850a03b10eadc11ac556c
ftconan <1508907793@qq.com>
2020-12-30 20:18:36 +0800

    go_note add variable

---
+++ go_note/ch1/variable/main.go
@@ -0,0 +1,35 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/12/30
+package main
+
+//var x int
+//var f float32 = 6
+//var s = "abc"
+
+func test() (int, string) {
+	return 1, "abc"
+}
+
+func main() {
+	x := 123
+	n, s := 0x1234, "Hello World!"
+	println(x, s, n)
+
+	data, i := [3]int{1, 2, 3}, 0
+	i, data[i] = 2, 100
+
+	_, s = test()
+	println(s)
+
+	s = "abc"
+	println(&s)
+	s, y := "hello", 20
+	println(&s, y)
+
+	{
+		s, z := 1000, 30
+		println(&s, z)
+	}
+
+}

5ff9478900fa3ca05df0225df0ec990f1bcb0e1b
ftconan <1508907793@qq.com>
2020-12-31 09:48:37 +0800

    go_note add enum

---
+++ go_note/ch1/enum/main.go
@@ -0,0 +1,52 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/12/31
+package main
+
+const (
+	Sunday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+const (
+	_        = iota
+	KB int64 = 1 << (10 * iota)
+	MB
+	GB
+	TB
+)
+const (
+	A = iota
+	B
+	C = "c"
+	D
+	E = iota
+	F
+)
+
+type Color int
+
+const (
+	Black Color = iota
+	Red
+	Blue
+)
+
+func test(c Color) {
+	println(c)
+}
+
+func main() {
+	c := Black
+	test(c)
+
+	//x := 1
+	//test(x)
+
+	test(1)
+}

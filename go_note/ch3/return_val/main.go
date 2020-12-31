22d3194d8334c1a4842901be405a253a43def9ab
ftconan <1508907793@qq.com>
2020-12-31 15:23:07 +0800

    go_note add return_val

---
+++ go_note/ch3/return_val/main.go
@@ -0,0 +1,65 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/12/31
+package main
+
+func test() (int, int)  {
+	return 1, 2
+}
+
+func add(x, y int) int  {
+	return x + y
+}
+
+func sum(n ...int) int  {
+	var x int
+	for _, i := range n {
+		x += i
+	}
+	return x
+}
+
+func add1(x, y int) (z int)  {
+	z = x + y
+	return
+}
+
+func add2(x, y int) (z int)  {
+	var z1 = x + y
+	return z1
+}
+
+func add3(x, y int) (z int)  {
+	defer func() {
+		z += 100
+	}()
+
+	z = x + y
+	return
+}
+
+func add4(x, y int) (z int)  {
+	defer func() {
+		println(z)
+	}()
+
+	z = x + y
+	return z + 200 // (z = z + 200) -> (call defer) -> (ret)
+}
+
+func main()  {
+	//s := make([]int, 2)
+	//s = test()
+
+	x, _ := test()
+	println(x)
+
+	println(add(test()))
+	println(sum(test()))
+
+	// add
+	println(add1(1, 2))
+	println(add2(1, 2))
+	println(add3(1, 2))
+	println(add4(1, 2))
+}

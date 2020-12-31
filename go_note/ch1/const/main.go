861c5fe4e1f524042644ac04f13983c2fbff07e5
ftconan <1508907793@qq.com>
2020-12-31 09:36:29 +0800

    go_note add const

---
+++ go_note/ch1/const/main.go
@@ -0,0 +1,31 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/12/31
+package main
+
+const x, y int = 1, 2
+const s = "Hello, World!"
+const (
+	a, b = 10, 100
+	c bool = false
+)
+//const (
+//	s = "abc"
+//	x
+//)
+//const (
+//	a = "abc"
+//	b = len(a)
+//	c = unsafe.Sizeof(a)
+//)
+//const (
+//	a byte = 100
+//	b int = 1e20
+//)
+
+func main()  {
+	//const x = "xxx"
+	println(x, y)
+	println(s)
+	println(a, b, c)
+}

1cf5e4d16d01cdaab1aebe3db4d7f6e7a4c0faf7
ftconan <1508907793@qq.com>
2020-12-31 10:47:00 +0800

    go_note add custom_type

---
+++ go_note/ch1/custom_type/main.go
@@ -0,0 +1,26 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/12/31
+package main
+
+import "fmt"
+
+func main() {
+	//var a struct{x int `a`}
+	//var b struct{x int `ab`}
+	//b = a
+
+	type bigint int64
+	var x bigint = 100
+	println(x)
+
+	x1 := 1234
+	var b bigint = bigint(x1)
+	var b2 int64 = int64(b)
+	println(b2)
+
+	type myslice []int
+	var s myslice = []int{1, 2, 3}
+	var s2 []int = s
+	fmt.Println(s2)
+}
---
+++ go_note/ch1/pointer/main.go
@@ -0,0 +1,49 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/12/31
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func test() *int {
+	x := 100
+	return &x
+}
+
+func main() {
+	type data struct {
+		a int
+	}
+	var d = data{1234}
+	var p *data
+	p = &d
+	fmt.Printf("%p, %v\n", p, p.a)
+
+	x := 1234
+	p1 := &x
+	fmt.Printf("%T\n", p1)
+	//p1++
+
+	x1 := 0x12345678
+	p2 := unsafe.Pointer(&x1) // *int -> Pointer
+	n := (*[4]byte)(p2)       // Pointer -> *[4]byte
+	for i := 0; i < len(n); i++ {
+		fmt.Printf("%X ", n[i])
+	}
+
+	fmt.Printf("%T\n", test())
+
+	d1 := struct {
+		s string
+		x int
+	}{"abc", 100}
+	p3 := uintptr(unsafe.Pointer(&d)) // *struct -> Pointer -> uintptr
+	p3 += unsafe.Offsetof(d1.x)       // uintptr + offset
+	p4 := unsafe.Pointer(p3)          //unitptr -> Pointer
+	px := (*int)(p4)                  // Pointer -> *int
+	*px = 200                         // d1.x = 200
+	fmt.Printf("%#v\n", d1)
+}

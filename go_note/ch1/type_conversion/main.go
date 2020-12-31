9afad58f1bb968b346ddcaf9f1eeed9f9852f9de
ftconan <1508907793@qq.com>
2020-12-31 10:06:42 +0800

    go_note add type_conversion

---
+++ go_note/ch1/type_conversion/main.go
@@ -0,0 +1,16 @@
+// Author: magician
+// File:   main.go
+// Date:   2020/12/31
+package main
+
+//var b byte = 100
+//var n int = b
+//var n int = int(b)
+
+func main()  {
+	a := 100
+	println(a)
+	//if a {
+	//	println("true")
+	//}
+}

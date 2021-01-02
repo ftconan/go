56c3ef89a3007eda3ffd7823b00515fcf65aebb7
ftconan <1508907793@qq.com>
2021-01-02 10:36:27 +0800

    go_note add method_anon

---
+++ go_note/ch5/method_anon/main.go
@@ -0,0 +1,31 @@
+// Author: magician
+// File:   main.go
+// Date:   2021/1/2
+package main
+
+import "fmt"
+
+type User struct {
+	id int
+	name string
+}
+
+type Manager struct {
+	User
+	title string
+}
+
+func (u *User) ToString() string  {
+	return fmt.Sprintf("User: %p, %v", u, u)
+}
+
+func (m *Manager) ToString() string  {
+	return fmt.Sprintf("Manager: %p, %v", m, m)
+}
+
+func main()  {
+	m := Manager{User{1, "Tom"}, "Administrator"}
+	//fmt.Printf("Manager: %p\n", &m)
+	fmt.Println(m.ToString())
+	fmt.Println(m.User.ToString())
+}

bd01fd3539f0d8af7dcbb05ea6fa2c01d64f8b64
ftconan <1508907793@qq.com>
2021-01-02 20:47:18 +0800

    go_note add interace_def

---
+++ go_note/ch6/interface_def/main.go
@@ -0,0 +1,49 @@
+// Author: magician
+// File:   main.go
+// Date:   2021/1/2
+package main
+
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+type Printer interface {
+	Stringer
+	Print()
+}
+
+type Tester struct {
+	s interface {
+		String() string
+	}
+}
+
+type User struct {
+	id   int
+	name string
+}
+
+func (u *User) String() string {
+	return fmt.Sprintf("user %d, %s", u.id, u.name)
+}
+
+func (u *User) Print() {
+	fmt.Println(u.String())
+}
+
+func Print(v interface{}) {
+	fmt.Printf("%T: %v\n", v, v)
+}
+
+func main() {
+	var t Printer = &User{1, "Tom"}
+	t.Print()
+
+	Print(1)
+	Print("Hello, World!")
+
+	//t := Tester{&User{1, "Tom"}}
+	//fmt.Println(t.s.String())
+}

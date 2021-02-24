2139e973896108be0088043d1048e79835e43fa6
ftconan <1508907793@qq.com>
2021-02-24 21:19:00 +0800

    add validator

--- gin_demo/main.go
+++ gin_demo/main.go
@@ -5,19 +5,39 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
-func main()  {
+type PostParams struct {
+	Name string `json:"name" uri:"name" form:"name"`
+	Age  int    `json:"age" uri:"age" form:"age" binding:"required,mustBig"`
+	Sex  bool   `json:"sex" uri:"sex" form:"sex"`
+}
+
+func mustBig(fl validator.FieldLevel) bool {
+	if fl.Field().Interface().(int) <= 18 {
+		return false
+	}
+	return true
+}
+
+func main() {
 	r := gin.Default() // Logger, Recovery (middleware)
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		_ = v.RegisterValidation("mustBig", mustBig)
+	}
+
 	r.GET("/path/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		user := c.DefaultQuery("user", "ft")
 		pwd := c.Query("pwd")
 
 		c.JSON(200, gin.H{
-			"id": id,
-			"user": user,
-			"pwd": pwd,
+			"id":     id,
+			"user":   user,
+			"pwd":    pwd,
 			"method": c.Request.Method,
 		})
 	})
@@ -26,9 +46,9 @@ func main()  {
 		pwd := c.PostForm("pwd")
 
 		c.JSON(200, gin.H{
-			"id": 1,
-			"user": user,
-			"pwd": pwd,
+			"id":     1,
+			"user":   user,
+			"pwd":    pwd,
 			"method": c.Request.Method,
 		})
 	})
@@ -38,9 +58,9 @@ func main()  {
 		pwd := c.PostForm("pwd")
 
 		c.JSON(200, gin.H{
-			"id": id,
-			"user": user,
-			"pwd": pwd,
+			"id":     id,
+			"user":   user,
+			"pwd":    pwd,
 			"method": c.Request.Method,
 		})
 	})
@@ -48,11 +68,27 @@ func main()  {
 		id := c.Param("id")
 
 		c.JSON(200, gin.H{
-			"id": id,
+			"id":     id,
 			"method": c.Request.Method,
 		})
 	})
+	r.POST("/testBind", func(c *gin.Context) {
+		var p PostParams
+		// ShouldBind 通过content-type判断
+		err := c.ShouldBindJSON(&p)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"msg":  "报错了",
+				"data": err.Error(),
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"msg":  "成功了",
+				"data": p,
+			})
+		}
+	})
 
-	//r.Run(":1010")
+	//r.Run(":8080")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/easierway/concurrent_map v0.0.0-20190103024436-7073b0dd7e95
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.2.0
 	github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75
 	github.com/smartystreets/goconvey v1.6.4
 	github.com/stretchr/testify v1.5.1

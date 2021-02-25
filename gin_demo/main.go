4a3cdb885c7a4e0108b13ee7cb44e63bbbf56c2f
ftconan <1508907793@qq.com>
2021-02-25 10:21:57 +0800

    upload file

--- gin_demo/main.go
+++ gin_demo/main.go
@@ -4,9 +4,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
+	"log"
 )
 
 type PostParams struct {
@@ -29,6 +31,7 @@ func main() {
 		_ = v.RegisterValidation("mustBig", mustBig)
 	}
 
+	// Using GET, POST, PUT, PATCH, DELETE and OPTIONS
 	r.GET("/path/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		user := c.DefaultQuery("user", "ft")
@@ -72,6 +75,7 @@ func main() {
 			"method": c.Request.Method,
 		})
 	})
+	// Model binding and validation
 	r.POST("/testBind", func(c *gin.Context) {
 		var p PostParams
 		// ShouldBind 通过content-type判断
@@ -86,6 +90,44 @@ func main() {
 				"msg":  "成功了",
 				"data": p,
 			})
+		}
+	})
+	// Upload files
+	r.POST("/testUpload", func(c *gin.Context) {
+		// Single file
+		//file, _ := c.FormFile("file")
+		//name := c.PostForm("name")
+		//dst := "./"+file.Filename
+		// Upload the file to specific dst.
+		//_ = c.SaveUploadedFile(file, dst)
+
+		// os
+		//src, _ := file.Open()
+		//defer src.Close()
+		//out, _ := os.Create(dst)
+		//defer out.Close()
+		//_, _ = io.Copy(out, src)
+
+		//c.Writer.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=%s", file.Filename))
+		//fmt.Println(file.Filename)
+		//c.File(dst)
+
+		//c.JSON(200, gin.H{
+		//	"msg": file,
+		//	"name": name,
+		//})
+
+		// Multiple file
+		form, _ := c.MultipartForm()
+		files := form.File["file"]
+
+		for _, file:= range files {
+			log.Println(file.Filename)
+			dst := "./"+file.Filename
+			// Upload the file to specific dst.
+			_ = c.SaveUploadedFile(file, dst)
+			c.Writer.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=%s", file.Filename))
+			c.File(dst)
 		}
 	})
 
